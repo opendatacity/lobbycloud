f4add65e3eb7c85c1a7ddeee07f133e150bf27b1
Sebastian Vollnhals <sebastian@vollnhals.info>
2014-03-31 16:49:35 +0200

    +beta site

---
+++ lobbycloud-beta.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+
+/* require node modules */
+var path = require("path");
+
+/* require npm modules */
+var express = require("express");
+var sqlite3 = require("sqlite3").verbose();
+var mustache_express = require("mustache-express");
+
+/* require config */
+var config = require(path.resolve(__dirname, "config.js"));
+
+/* sugnup database */
+var signupdb = new sqlite3.Database(path.resolve(__dirname, config.signupdb));
+
+/* launche express */
+var app = express();
+
+/* configure */
+app.engine('mustache', mustache_express());
+
+app.use(express.bodyParser());
+
+app.set('view engine', 'mustache');
+app.set('views', path.resolve(__dirname, 'assets/views'));
+
+/* serve assets */
+app.use('/assets', express.static(__dirname + '/assets'));
+
+/* routes */
+app.get('/', function(req, res){
+	res.render('beta', {
+		"url": "http://localhost:3000",
+		"invite": true
+	});
+});
+
+app.post('/signup-beta', function(req, res){
+	signupdb.run("INSERT INTO signup (date, name, email, motivation) VALUES (?, ?, ?, ?);", [
+		parseInt((new Date()).getTime()/1000,10),
+		req.body.name,
+		req.body.email,
+		req.body.motivation
+	], function(err){
+		res.render('beta', {
+			"url": "http://localhost:3000",
+			"thankyou": (err === null),
+			"error": (err !== null)
+		});
+	});
+});
+
+app.all('*', function(req, res){
+	res.redirect('/');
+});
+
+/* listen */
+if (config.listen.hasOwnProperty("socket")) {
+	app.listen(config.listen.socket, function(){
+		console.log("server listening on socket", config.listen.socket);
+	});	
+} else if (config.listen.hasOwnProperty("host")){
+	app.listen(config.listen.host, config.listen.port, function(){
+		console.log("server listening on", [config.listen.host, config.listen.port].join(":"));
+	});	
+} else {
+	app.listen(config.listen.port, function(){
+		console.log("server listening on", ["*", config.listen.port].join(":"));
+	});
+}
