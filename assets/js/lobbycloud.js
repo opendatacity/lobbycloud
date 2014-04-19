9f214398bfd0eedfbcd029d53776e576e381cf56
Sebastian Vollnhals <sebastian@vollnhals.info>
2014-04-19 11:24:24 +0200

    topic input selector yay

--- assets/js/lobbycloud.js
+++ assets/js/lobbycloud.js
@@ -1,6 +1,7 @@
 $(document).ready(function(e){
 	$('#main').css('min-height', $(window).innerHeight()-($('header').outerHeight()+$('footer').outerHeight()));
 	
+	/* strengthometer */
 	$("input[type=password].strengthometer").each(function(idx,e){
 		var $i = $(e);
 		var $m = $('<div class="progress password-progress"><div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="5" style="width: 0%;"><span class="sr-only">0% Complete</span></div></div>');
@@ -18,6 +19,67 @@ $(document).ready(function(e){
 			}
 		});
 	});
+	
+	/* topic select */
+	$('.form-topic').each(function(idx,e){
+		var $e = $(this);
+		var $v = $("input[name=topic]", $e);
+		var $q = $("input[name=query]", $e);
+		$q.typeahead({
+		  minLength: 3,
+		  highlight: true
+		},{
+			displayKey: "label",
+			source: function(query, callback) {
+				$.get("/api/topics/suggest", {q: query}, callback, "json");
+			}
+		});
+		var _activate = function(o,v){
+			$q.typeahead("close");
+			$v.val(v.id);
+			$q.attr("readonly","readonly").addClass("fixed").one("click", function(evt){
+				$q.val("").removeAttr("readonly").removeClass("fixed");
+				$v.val("");
+			});
+		};
+		$q.on("typeahead:selected", _activate);
+		$q.on("typeahead:autocompleted", _activate);
+		
+		$(".dropdown-action .dropdown-menu li a", $e).each(function(idx,f){
+			var $f = $(this);
+			$f.click(function(evt){
+				evt.preventDefault();
+
+				/* show text on dropdown button */
+				$(".dropdown-label", $e).text($f.text());
+				
+				/* do magic */
+				switch ($f.attr("data-action")) {
+					case "disabled":
+						$q.attr("type","hidden");
+						$v.val("").attr("disabled", "disabled").attr("type","text");
+					break;
+					case "select":
+						$q.val("").removeAttr("readonly").removeClass("fixed").attr("type","text");
+						$v.val("").removeAttr("disabled").attr("type","hidden");
+					break;
+					case "suggest":
+						$q.attr("type","hidden");
+						$v.val("").removeAttr("disabled").attr("type","text").focus();
+					break;
+				}
+				
+			});
+		});
+		
+	});
+
+	/*
+	$('.dropdown-menu li').click(function(e){
+	  var selected = $(this).text();
+	  $('.category').val(selected);  
+	});
+	*/
 	
 });
 
